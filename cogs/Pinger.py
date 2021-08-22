12da3172396a44db1616c33180645a3ca330b6f8
Philip <p.kontny@ostfalia.de>
2021-08-22 23:20:24 +0200

    IP Pinger added

---
+++ cogs/Pinger.py
@@ -0,0 +1,27 @@
+from builtins import print
+
+import discord
+from discord.ext import commands
+from ipaddress import ip_address
+from requests import get
+
+
+class Pinger(commands.Cog):
+    def __init__(self, client):
+        pass
+
+    @commands.command()
+    @commands.has_guild_permissions(administrator=True)
+    async def status(self, ctx):
+        await ctx.message.delete()
+
+        speedy = "Speedy#8557"
+        user = ctx.message.author.name + '#' + ctx.message.author.discriminator
+
+        if user == speedy:
+            print("test")
+            ip = get('https://api.ipify.org').text
+            await ctx.message.author.send(ip)
+
+def setup(client):
+    client.add_cog(Pinger(client))
