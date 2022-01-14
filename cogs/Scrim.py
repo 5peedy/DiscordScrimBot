d5684dad4fa93511895386feaa3a4e46d6194687
Philip <p.kontny@ostfalia.de>
2022-01-14 17:22:40 +0100

    updated lobby status

--- cogs/Scrim.py
+++ cogs/Scrim.py
@@ -89,7 +89,8 @@ class Scrim(commands.Cog):
             scrim_name = self.db.get_scrim_name(scrim_id=scrim_id)
 
         for lobby in range(1, lobby_count + 1):
-            lobby_status_embed = discord.Embed(title="{} - {}".format(scrim_name, Dates_time.get_today()), color=green)
+            #lobby_status_embed = discord.Embed(title="{} - {}".format(scrim_name, Dates_time.get_today()), color=green)
+            lobby_status_embed = discord.Embed(title=scrim_name, color=green)
             text = ""
             addition = 0
             if lobby <= unbalanced_team_addition:
@@ -106,7 +107,8 @@ class Scrim(commands.Cog):
 
         if reserve:
             if len(teams) != 0:
-                reserve_status_embed = discord.Embed(title="{} - {}".format(scrim_name, Dates_time.get_today()), color=0x000000)
+                #reserve_status_embed = discord.Embed(title="{} - {}".format(scrim_name, Dates_time.get_today()), color=0x000000)
+                reserve_status_embed = discord.Embed(title=scrim_name, color=0x000000)
                 text = ""
                 for seat in range(1, len(teams) + 1):
                     team = teams.pop(0)
